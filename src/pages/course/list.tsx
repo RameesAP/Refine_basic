e6d83ebfcc0579d1ec892835c78b3347b2cc9023
RameesAP <rameeswork@gmail.com>
2023-07-24 17:56:36 +0530

    Modal Tested

--- src/pages/course/list.tsx
+++ src/pages/course/list.tsx
@@ -9,12 +9,29 @@ import {
   DeleteButton,
   CreateButton,
 } from "@refinedev/antd";
+import { useState } from "react";
+import { Button } from "antd/es/radio";
+import Modal from "antd/es/modal/Modal";
 
 export const CourseList: React.FC<IResourceComponentsProps> = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   const { tableProps, sorters } = useTable({
     resource: "course",
     meta: {
-      fields: ["id", "name", "age"],
+      fields: ["id", "name", "age"]
     },
     sorters: {
       initial: [
@@ -28,7 +45,28 @@ export const CourseList: React.FC<IResourceComponentsProps> = () => {
   return (
     <>
       <List>
-        <CreateButton resource="course" />
+
+        {/* Modal */}
+
+        <Space>
+          <CreateButton resource="course" />
+          <Button type="primary" onClick={showModal}>
+            Open Modal
+          </Button>
+        </Space>
+        <Modal
+          title="This is My Modal"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <p>
+            Hello This is Ramees
+          </p>
+          <p>How are you</p>
+          <p>hahaahaa its worked</p>
+        </Modal>
+
         <Table {...tableProps} rowKey="id">
           <Table.Column dataIndex="id" title="ID" />
           <Table.Column
